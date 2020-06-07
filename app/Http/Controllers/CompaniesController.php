5cd7639e3117708644cfe03838680ffdbc3f999d
Vinícius Bohrer <vbsanttos@gmail.com>
2020-06-07 18:48:26 -0300

    fix and clean controllers

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -8,44 +8,32 @@ use App\Company;
 
 class CompaniesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         Log::info("CompaniesController@index");
+
         $companies = Company::paginate(10);
         return view('companies.index')->with("companies", $companies);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         Log::info("CompaniesController@create");
+
         return view('companies.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         Log::info("CompaniesController@store: ". json_encode($request->all()));
+
         $request->validate([
             'name' => ['required','max:255'],
             'email' => ['required','unique:App\Company,email','email:rfc,dns','max:255'],
             'site' => ['url','max:255'],
             'logo' => ['dimensions:min_width=100, min_height=100'],
         ]);
+
         $company = new Company();
         $company->name = $request->input('name');
         $company->email = $request->input('email');
@@ -63,67 +51,56 @@ class CompaniesController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         Log::info("CompaniesController@show: ".$id);
+
         $company = Company::findOrFail($id);
         return $company;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         Log::info("CompaniesController@edit: ".$id);
+
         $company = Company::findOrFail($id);
-        return view('companies.edit')->with("companies", $companies);
+        return view('companies.edit')->with("company", $company);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         Log::info("CompaniesController@update: [".$id."] ".json_encode($request->all()));
+
         $request->validate([
             'name' => ['required','max:255'],
-            'email' => ['required','unique:App\Company,email','email:rfc,dns','max:255'],
+            'email' => ['required','email:rfc,dns','max:255'],
             'site' => ['url','max:255'],
             'logo' => ['dimensions:min_width=100, min_height=100'],
         ]);
+
         $company = Company::findOrFail($id);
         $company->name = $request->name;
         $company->email = $request->email;
         $company->site = $request->site;
-        $company->logo = $request->logo;
+        $logo = $request->file('logo');
+        if (isset($logo)) {
+            // $oldImage = $company->logo; // REVIEW remover imagem anterior?
+            $path = $request->file('logo')->store('images', 'public');
+            $company->logo = $path;
+            $company->save();
+        }
         $company->save();
-        return $company;
+        if (isset($company)) {
+            return redirect('/companies');
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function delete($id)
     {
         Log::info("CompaniesController@delete: ".$id);
+
         $company = Company::findOrFail($id);
-        return $company->delete();
+        $company->delete();
+        return redirect('/companies');
     }
 }
--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -4,45 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
 use App\Employee;
 use App\Company;
 
 class EmployeesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
         Log::info("EmployeesController@index");
         $employees = Employee::paginate(10);
-        return view('employees.index')->with("employees", $employees);
+        $companies = Company::all();
+        return view('employees.index')->with("employees", $employees)->with("companies", $companies);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         Log::info("CompaniesController@create");
+
         $companies = Company::all();
-        return view('employees.create')->with("companies", $companies);;
+        return view('employees.create')->with("companies", $companies);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         Log::info("EmployeesController@store: ". json_encode($request->all()));
+
         $request->validate([
             'firstname' => ['required','max:255'],
             'lastname' => ['required','max:255'],
@@ -50,70 +36,55 @@ class EmployeesController extends Controller
             'email' => ['required','unique:App\Employee,email','email:rfc,dns','max:255'],
             'phone' => ['min:8','max:15'],
         ]);
+
         $employee = Employee::create($request->all());
         if (isset($employee)) {
             return redirect('/employees');
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         Log::info("EmployeesController@show: ".$id);
+
         $employee = Employee::findOrFail($id);
         return $employee;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         Log::info("EmployeesController@edit: ".$id);
+
         $employee = Employee::findOrFail($id);
-        return view('employees.edit')->with("employees", $employees);
+        $companies = Company::all();
+        return view('employees.edit')->with("companies", $companies)->with("employee", $employee);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         Log::info("EmployeesController@update: [".$id."] ".json_encode($request->all()));
+
         $request->validate([
             'firstname' => ['required','max:255'],
             'lastname' => ['required','max:255'],
             'company_id' => ['required','exists:App\Company,id','max:255'],
-            'email' => ['required','unique:App\Employee,email','email:rfc,dns','max:255'],
-            'phone' => ['min:8','max:15'],
+            'email' => ['required','email:rfc,dns','max:255'],
+            'phone' => ['min:8','max:15',],
         ]);
+
         $employee = Employee::findOrFail($id);
         $employee->update($request->all());
-        return $employee;
+        if (isset($employee)) {
+            return redirect('/employees');
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function delete($id)
     {
         Log::info("EmployeesController@delete: ".$id);
+
         $employee = Employee::findOrFail($id);
-        return $employee->delete();
+        $employee->delete();
+        return redirect('/employees');
     }
 }
