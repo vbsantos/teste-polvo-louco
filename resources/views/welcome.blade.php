93b416b5fc0a2d6248fe6df67b3ed7cce24002d7
Vinícius Bohrer <vbsanttos@gmail.com>
2020-06-07 18:54:33 -0300

    finish basic and auth related views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,22 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Teste Polvo Louco</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
             .full-height {
                 height: 100vh;
             }
@@ -49,7 +41,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #464b4e;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
@@ -65,10 +57,16 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Teste Polvo Louco
+                </div>
+                @if (Route::has('login'))
+                <div class="flex-center links">
                     @auth
-                        <a href="{{ url('/dashboard') }}">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}">Go To Dashboard</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
@@ -78,22 +76,6 @@
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
